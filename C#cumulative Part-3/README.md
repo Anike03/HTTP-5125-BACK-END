1d519da261a4e2965d949474dceb4beedb87abd2
Aniket Sharma <n01667327@humber.ca>
2024-12-13 20:03:48 -0500

    Delete C#cumulative Part-3 directory

--- C#cumulative Part-3/README.md
+++
@@ -1,1 +0,0 @@
-Cumulative Part-3
--- C#cumulative Part-3/SchoolDatabase/Models/TeacherSearchModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolDatabase.Models
-{
-    public class TeacherSearchModel
-    {
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
-        public List<Teacher> Teachers { get; set; }
-    }
-}
