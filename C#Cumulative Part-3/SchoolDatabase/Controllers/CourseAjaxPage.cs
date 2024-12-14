d76b0a7c408fe81de72769dd4113d9bca61981f5
Aniket Sharma <n01667327@humber.ca>
2024-12-13 20:09:00 -0500

    Add files via upload

---
+++ C#Cumulative Part-3/SchoolDatabase/Controllers/CourseAjaxPage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolDatabase.Controllers
+{
+    public class CourseAjaxPage : Controller
+    {
+       
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
